358b3cb8b5f4ff2bed2d5f014ee815679ba93e1e
Igors Krasjukovs <tahq69@gmail.com>
2017-11-26 17:43:38 +0200

    [build] v1.0.2

--- release.bat
+++ release.bat
@@ -1,13 +1,19 @@
 :: Build assets
-:: npm run build
+call npm run build
+
+echo Build completed
 
 :: Commit sources to git repository
 git add -A
 git commit -m "[build] v%1"
 
 :: Update version number
-npm version %1 --message "[release] v%1"
+call npm version %1 --message "[release] v%1"
+
+echo Version updated
 
 :: Publish
-git push
-npm publish
+call git push
+call npm publish
+
+echo Publish done
