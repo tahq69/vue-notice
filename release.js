9fb7cd8051a1db1c9d70d4ac402e8522c831f44d
Igors Krasjukovs <tahq69@gmail.com>
2017-12-10 21:11:20 +0200

    Build log cleanup;

--- release.js
+++ release.js
@@ -1,19 +1,18 @@
-var spawn = require("child_process").spawn
+let spawn = require("child_process").spawn
 let version = require("./package.json").version
+
 let parts = version.split(".")
 let last = parts.splice(-1, 1)[0]
 version = parts.join(".") + "." + (parseInt(last || 0) + 1)
 
-var ls = spawn("cmd.exe", ["/c", `release.bat ${version}`])
+console.log(`Build of v${version} started.`)
 
-ls.stdout.on("data", function(data) {
-  console.log("stdout: " + data)
+let ls = spawn("cmd.exe", ["/c", `release.bat ${version}`])
+
+ls.stdout.on("data", data => {
+  console.log(data)
 })
 
-ls.stderr.on("data", function(data) {
-  console.log("stderr: " + data)
-})
-
-ls.on("exit", function(code) {
-  console.log("child process exited with code " + code)
+ls.on("exit", code => {
+  console.log(`process exited with code ${code}`)
 })
