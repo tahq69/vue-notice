398c882a01009c9610b93ee88d43f2ef1f898b7a
Igors Krasjukovs <tahq69@gmail.com>
2017-12-30 16:03:29 +0200

    Adding builds;

--- build/release.js
+++ build/release.js
@@ -1,5 +1,5 @@
 let spawn = require("child_process").spawn
-let version = require("./package.json").version
+let version = require("./../package.json").version
 
 let parts = version.split(".")
 let last = parts.splice(-1, 1)[0]
@@ -7,10 +7,14 @@ version = parts.join(".") + "." + (parseInt(last || 0) + 1)
 
 console.log(`Build of v${version} started.`)
 
-let ls = spawn("cmd.exe", ["/c", `release.bat ${version}`])
+let ls = spawn("cmd.exe", ["/c", `\.\\build\\release.bat ${version}`])
 
 ls.stdout.on("data", data => {
-  console.log(data)
+  console.log(data.toString())
+})
+
+ls.stderr.on("data", function(data) {
+  console.warn(data.toString())
 })
 
 ls.on("exit", code => {
---
+++ build/webpack.config.js
@@ -0,0 +1,133 @@
+const path = require("path")
+const webpack = require("webpack")
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+
+let version = require("./../package.json").version
+let parts = version.split(".")
+let last = parts.splice(-1, 1)[0]
+version = parts.join(".") + "." + (parseInt(last || 0) + 1)
+
+console.log(`Creating documentation build of v${version}:`)
+
+let resolve = relativePath => path.resolve(__dirname, "./..", relativePath)
+
+module.exports = {
+  entry: {
+    app: resolve("app/main.ts"),
+  },
+  output: {
+    path: resolve("./dist"),
+    publicPath: "/dist/",
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js", ".vue", ".json"],
+    alias: {
+      "@": resolve("src"),
+      "#": resolve("app"),
+      "$": resolve("types"),
+      "&": resolve("test"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.ts$/,
+        loader: "tslint-loader",
+        exclude: /node_modules|vue\/src|vendor\/*/,
+        options: {
+          configFile: resolve("tslint.json"),
+        },
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules|vue\/src|vendor\/*/,
+        loader: "ts-loader",
+        include: [resolve("src"), resolve("app")],
+        options: { appendTsSuffixTo: [/\.vue$/] },
+      },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+        options: {
+          loaders: {
+            scss: "vue-style-loader!css-loader!sass-loader",
+            sass: "vue-style-loader!css-loader!sass-loader?indentedSyntax",
+            esModule: true,
+          },
+        },
+      },
+      {
+        test: /\.ts$/,
+        loader: "string-replace-loader",
+        query: { search: "__VERSION__", replace: version },
+      },
+      {
+        test: /\.scss$|\.css$/,
+        exclude: /node_modules/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "sass-loader"],
+        }),
+      },
+      {
+        test: /\.woff($|\?)|\.woff2($|\?)|\.ttf($|\?)|\.eot($|\?)|\.svg($|\?)/,
+        loader: "url-loader",
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: "file-loader",
+        options: { name: "[name].[ext]?[hash]" },
+      },
+    ],
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+  },
+  performance: {
+    hints: false,
+  },
+  devtool: "#eval-source-map",
+  plugins: [
+    new ExtractTextPlugin("styles.css"),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      progress: true,
+      hide_modules: true,
+    }),
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV || "development"),
+    }),
+    new webpack.BannerPlugin({
+      banner: `/*!
+* Crip Vue Notice v${version}
+* Forged by Igors Krasjukovs <tahq69@gmail.com>
+* Released under the MIT License.
+*/`,
+      raw: true,
+      entryOnly: true,
+    }),
+  ],
+}
+
+if (process.env.NODE_ENV !== "test") {
+  module.exports.externals = {
+    vue: "Vue",
+    "vue-router": "VueRouter",
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
+  module.exports.devtool = "#source-map"
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      },
+    }),
+  ])
+}
