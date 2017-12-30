c3047d9a4a87f8f5b9a700c6ba9bcfcf3f7c695c
Igors Krasjukovs <tahq69@gmail.com>
2017-12-30 19:40:23 +0200

    [build] v1.1.0

--- src/settings.ts
+++ src/settings.ts
@@ -5,11 +5,11 @@ export default {
   closable: true,
   duration: 4.5,
   icons: {
+    close: "fa fa-times",
+    error: "fa fa-times-circle",
     info: "fa fa-info-circle",
     success: "fa fa-check-circle",
     warning: "fa fa-exclamation-circle",
-    error: "fa fa-times-circle",
-    close: "fa fa-times",
   },
   styles: { top: "15px", right: "15px" },
 } as NoticesOptions
